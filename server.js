fd8938e114b42ae20eb6b9dc8ec511bc1358852e
aliceajameson7-lgtm <aliceajameson7@gmail.com>
2025-09-19 17:16:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import router from './userTime.js';
 import geminiRouter from './geminiRoute.js';
+import session from 'express-session';
 const app = express();
 const __filename =fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
 app.use(express.json());
-
+app.use(session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
 app.use('/randomGeneration', geminiRouter);
 app.use('/times', router);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,3 +21,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.listen(8000, () => console.log('Server connected'));
+
