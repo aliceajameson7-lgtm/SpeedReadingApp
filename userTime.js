d3ad1caa1f71395ddde2b7e498ce648f2c2832eb
aliceajameson7-lgtm <aliceajameson7@gmail.com>
2025-09-19 16:18:15 -0500

    Update userTime.js

--- userTime.js
+++ userTime.js
@@ -1,18 +1,26 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 
-let times = [];
-router.post('/', async (req, res) => {
+// Save time per user session
+router.post("/", (req, res) => {
+  if (!req.session.times) {
+    req.session.times = [];
+  }
+
   const { seconds } = req.body;
-  const newTime = { time: seconds, id: times.length + 1 };
-  times.push(newTime)
-  res.status(201).json(times);
+  const newTime = { time: seconds, id: req.session.times.length + 1 };
+  req.session.times.push(newTime);
+
+  res.status(201).json(req.session.times);
 });
-router.get('/', (req, res) => {
-  res.status(201).json(times);
-})
-router.get('/Truncate', (req, res) => {
-  times = [];
-  res.status(201).json(times);
+
+router.get("/", (req, res) => {
+  res.status(200).json(req.session.times || []);
 });
+
+router.get("/truncate", (req, res) => {
+  req.session.times = [];
+  res.status(200).json(req.session.times);
+});
+
 export default router;
